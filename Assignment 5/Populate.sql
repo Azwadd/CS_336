ab1a420efbf9f52eb1c0096dc5b986b905c27c27
azwad <shameemazwad@gmail.com>
2021-11-27 19:02:21 -0500

    Update

--- Assignment 5/Populate.sql
+++ Assignment 5/Populate.sql
@@ -1,8 +1,6 @@
-SET @GLOBAL.local_infile = 1;
-USE Transportation;
-
-DROP TABLE IF EXISTS CitiBike;
-CREATE TABLE CitiBike(
+DROP TABLE IF EXISTS dbo.CitiBike;
+GO
+CREATE TABLE dbo.CitiBike(
     TripDuration FLOAT,
     StartTime VARCHAR(30),
     StartDay VARCHAR(9) DEFAULT NULL,
@@ -21,11 +19,13 @@ CREATE TABLE CitiBike(
     Birth VARCHAR(4),
     Gender INT
 );
+BULK INSERT dbo.CitiBike FROM 'C:\Users\azwad\IdeaProjects\CS_336\Assignment 5\data.txt'
+     WITH (FIELDTERMINATOR  = '\t', ROWTERMINATOR = '\n', FIRSTROW = 2)
 
-SELECT * FROM CitiBike;
-#INSERT INTO CitiBike FROM 'data.txt'
-#CREATE TABLE STATION(Id INT, Name VARCHAR(64), Latitude DOUBLE, Longitude DOUBLE)
-#CREATE TABLE TRIPS(StationId INT, MinTripDuration, MaxTripDuration, AvgTripDuration, NumberUsers INT)
-#UsageByDay(StationId, NumberUsersWeekday, NumberUsersWeekend)
-#UsageByGender(StationId, NumberMaleUsers, NumberFemaleUsers)
-#UsageByAge(StationId, NumberUsersUnder18, NumberUsers18To40, NumberUsersOver40)
+SELECT * FROM dbo.CitiBike;
+-- INSERT INTO CitiBike FROM 'data.txt'
+-- #CREATE TABLE STATION(Id INT, Name VARCHAR(64), Latitude DOUBLE, Longitude DOUBLE)
+-- #CREATE TABLE TRIPS(StationId INT, MinTripDuration, MaxTripDuration, AvgTripDuration, NumberUsers INT)
+-- #UsageByDay(StationId, NumberUsersWeekday, NumberUsersWeekend)
+-- #UsageByGender(StationId, NumberMaleUsers, NumberFemaleUsers)
+-- #UsageByAge(StationId, NumberUsersUnder18, NumberUsers18To40, NumberUsersOver40)
